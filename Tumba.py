8122f99e69d03e2bbc09bb3acbb223ff5e79dece
MrPizvion <mrpizvion@gmail.com>
2026-03-02 10:01:20 +0700

    Update Tumba.py

--- Tumba.py
+++ Tumba.py
@@ -6,31 +6,35 @@ logger = logging.getLogger(__name__)
 
 @loader.tds
 class TumbochkaMod(loader.Module):
-    """Фан-модуль Тумбочка (4 в 1)"""
+    """Фан-модуль Тумбочка (4 в 1) с русскими командами"""
     strings = {"name": "Tumbochka Fun"}
 
-    async def tумбочкаcmd(self, message):
+    @loader.command(ru_doc="Просто тумбочка")
+    async def тумбочкаcmd(self, message):
         """Просто тумбочка"""
         await utils.answer(message, "<b>🪑 Тумбочка</b>\n(самая обычная, без фото)")
 
-    async def tумбаcmd(self, message):
+    @loader.command(ru_doc="Тумба (без фото)")
+    async def тумбаcmd(self, message):
         """Тумба (без фото)"""
         await utils.answer(message, "<b>🗄️ Тумба</b>\n(тоже без фото, просто тумба)")
 
-    async def tумба_рандомcmd(self, message):
+    @loader.command(ru_doc="Тумба с рандомным фото")
+    async def тумба_рандомcmd(self, message):
         """Тумба с рандомным фото"""
         photos = [
-            "https://i.imgur.com/6W8xM6j.jpg",  # обычная тумба
-            "https://i.imgur.com/3LZqX2b.jpg",  # деревянная тумба
-            "https://i.imgur.com/K9ZkQrW.jpg",  # современная тумба
-            "https://i.imgur.com/Q5p3cUf.jpg",  # тумба с ящиками
-            "https://i.imgur.com/RtV4sMp.jpg",  # белая тумба
+            "https://i.imgur.com/6W8xM6j.jpg",
+            "https://i.imgur.com/3LZqX2b.jpg",
+            "https://i.imgur.com/K9ZkQrW.jpg",
+            "https://i.imgur.com/Q5p3cUf.jpg",
+            "https://i.imgur.com/RtV4sMp.jpg",
         ]
         photo = random.choice(photos)
         caption = "<b>📸 Рандомная тумба</b>\n(фото сгенерировано случайно)"
         await utils.answer(message, caption, photo=photo)
 
-    async def tумба_18cmd(self, message):
+    @loader.command(ru_doc="Тумба 18+ (обычное тумбочка)")
+    async def тумба_18cmd(self, message):
         """Тумба 18+ (обычное тумбочка)"""
         await utils.answer(
             message,
